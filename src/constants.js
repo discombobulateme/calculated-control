4e14a4f02df63d61dafd98103c1b4ef5e503a2ee
jakubfiala <j4kub.fiala@gmail.com>
2021-03-06 11:10:36 +0100

    new api key

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const API_BASE_URL = 'https://api.zotero.org';
-export const API_KEY = 'ndOdeAUS2uyS7ElQUdjW0bdf';
+export const API_KEY = 'wJP6BSWTorMeph7NhXsOQIHl';
 export const GROUP_ID = '2468861';
