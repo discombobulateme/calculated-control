aefe46252b630bcc1a6865cc6edab475b3f0657b
discombobulateme <p@discombobulate.me>
2021-04-13 23:34:00 +0200

    Change date format for HDS and panke to improve exhibition menu

--- src/archive-config.js
+++ src/archive-config.js
@@ -34,50 +34,50 @@ export const getAboutConfig = (node, tags = []) => {
 };
 
 export const datePickerConfig = {
-  symposium: {
-    dates: [
-      {
-        humanReadable: '01/05/2021',
-        tag: 'date_2021_05_01',
-      },
-      {
-        humanReadable: '02/05/2021',
-        tag: 'date_2021_05_02',
-      },
-      {
-        humanReadable: '03/05/2021',
-        tag: 'date_2021_05_03',
-      },
-    ],
-  },
-  ausstellung: {
-    reset: false,
-    default: 'date_2021_04_30',
-    dates: [
-      {
-        humanReadable: 'Haus der Statistik 30/04 – 09/05/2021',
-        tag: 'date_2021_04_30',
-      },
-      {
-        humanReadable: 'panke.gallery 18/09/ – 03/10/2021',
-        tag: 'date_2021_09_18',
-      }
-    ],
-  },
-  unconference: {
-    dates: [
-      {
-        humanReadable: '21/05/2021',
-        tag: 'date_2021_05_21',
-      },
-      {
-        humanReadable: '22/05/2021',
-        tag: 'date_2021_05_22',
-      },
-      {
-        humanReadable: '23/05/2021',
-        tag: 'date_2021_05_23',
-      },
-    ],
-  },
-}
+         symposium: {
+           dates: [
+             {
+               humanReadable: '01/05/2021',
+               tag: 'date_2021_05_01',
+             },
+             {
+               humanReadable: '02/05/2021',
+               tag: 'date_2021_05_02',
+             },
+             {
+               humanReadable: '03/05/2021',
+               tag: 'date_2021_05_03',
+             },
+           ],
+         },
+         ausstellung: {
+           reset: false,
+           default: 'date_2021_04_30',
+           dates: [
+             {
+               humanReadable: 'Haus der Statistik 30/04 – 09/05',
+               tag: 'date_2021_04_30',
+             },
+             {
+               humanReadable: 'panke.gallery 18/09 – 03/10',
+               tag: 'date_2021_09_18',
+             },
+           ],
+         },
+         unconference: {
+           dates: [
+             {
+               humanReadable: '21/05/2021',
+               tag: 'date_2021_05_21',
+             },
+             {
+               humanReadable: '22/05/2021',
+               tag: 'date_2021_05_22',
+             },
+             {
+               humanReadable: '23/05/2021',
+               tag: 'date_2021_05_23',
+             },
+           ],
+         },
+       };
