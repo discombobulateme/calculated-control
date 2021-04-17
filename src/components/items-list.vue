6cb716a8d5b19cb7618ab8e1ab6dc2b7beb5eb63
discombobulateme <p@discombobulate.me>
2021-04-17 10:00:42 +0200

    Change padding to fix iPhone issue

--- src/components/items-list.vue
+++ src/components/items-list.vue
@@ -64,12 +64,7 @@ export default {
     grid-template-columns: repeat(2, 1fr);
   }
 
-  .items-list__about {
-    min-height: calc(100vh - 80px);
-  }
-
   .items-list__item {
-    /* min-height: calc(100vh - 80px); */
     min-height: 288px;
     height: 100%;
   }
@@ -98,7 +93,7 @@ export default {
 }
 
 .items-list__about {
-  padding: 5px;
+  padding: 2px;
 }
 
 .items-list__about-link {
@@ -109,4 +104,5 @@ export default {
   height: 100%;
   text-decoration: none;
 }
+
 </style>
