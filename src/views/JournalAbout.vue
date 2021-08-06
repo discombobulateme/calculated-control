e6a6aa78d938794f0900b28b9ecb7df1c31054c2
discombobulateme <p@discombobulate.me>
2021-08-06 15:27:57 +0200

    Delete journal publishing dates

--- src/views/JournalAbout.vue
+++ src/views/JournalAbout.vue
@@ -1,14 +1,25 @@
 <template>
   <Page>
     <template #meta>
-      <h1 class="journal-about__title">{{ english ? 'Accuracy and Obsession' : 'Akribie und Obsession' }}</h1>
+      <h1 class="journal-about__title">
+        {{ english ? 'Accuracy and Obsession' : 'Akribie und Obsession' }}
+      </h1>
     </template>
     <template #content>
       <template v-if="english">
-        Cybernetic control is achieved through accuracy, meticulousness, and a constant striving for perfection. Social processes are broken down into manageable parts, measured, and evaluated in order to regulate them by targeted readjustment. The authors of the online journal examine concrete practices, technologies, and materials of cybernetic control. The essays will be published from May to July 2021.
+        Cybernetic control is achieved through accuracy, meticulousness, and a
+        constant striving for perfection. Social processes are broken down into
+        manageable parts, measured, and evaluated in order to regulate them by
+        targeted readjustment. The authors of the online journal examine
+        concrete practices, technologies, and materials of cybernetic control.
       </template>
       <template v-else>
-        Kybernetische Kontrolle wird durch Genauigkeit, Akribie und einem beständigen Streben nach Perfektion erlangt. Um gesellschaftliche Prozesse durch zielgerichtetes Nachjustieren zu regulieren, werden sie in überschaubare Einzelteile zerlegt, gemessen und bewertet. Die Autor*innen des online Journals untersuchen konkrete Praktiken, Technologien und Materialien der kybernetischen Kontrolle. Die Essay werden von Mais bis Juli 2021 hier veröffentlicht.
+        Kybernetische Kontrolle wird durch Genauigkeit, Akribie und einem
+        beständigen Streben nach Perfektion erlangt. Um gesellschaftliche
+        Prozesse durch zielgerichtetes Nachjustieren zu regulieren, werden sie
+        in überschaubare Einzelteile zerlegt, gemessen und bewertet. Die
+        Autor*innen des online Journals untersuchen konkrete Praktiken,
+        Technologien und Materialien der kybernetischen Kontrolle.
       </template>
     </template>
   </Page>
