91435b2d13fa1c851a0804f8a8533e0f9c8e44b3
discombobulateme <p@discombobulate.me>
2021-08-06 15:28:10 +0200

    Add Paulina Thom in the team

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -11,20 +11,127 @@
       </div>
       <div class="team__section-centre">
         <template v-if="english">
-          <p>Zentrum für Netzkunst reconstructs, maintains, and preserves netart and net culture. The association researches archiving and contextualization of netart. Since 2019, we are based at the Haus der Statisik, where we are conducting research on the history of the building and its former functions.</p>
-          <p>Members of the association are: <a class="link" target="_blank" rel="noopener" href="https://wwwwwwwwwwwwwwwwwwwwww.bitnik.org/">!Mediengruppe Bitnik</a>, Tereza Havlíková, <a class="link" target="_blank" rel="noopener" href="http://www.discombobulate.me/en/">Paloma Oliveira</a>, <a href="https://super-filme.org/" target="_blank" rel="noopener" class="link">Anneliese Ostertag</a>, <a class="link" target="_blank" rel="noopener" href="https://super-filme.org/">Tabea Rossol</a>, <a class="link" target="_blank" rel="noopener" href="http://www.curatingyoutube.net/">Sakrowski</a>, and <a class="link" target="_blank" rel="noopener" href="http://artwarez.org/">Cornelia Sollfrank</a>.</p>
+          <p>
+            Zentrum für Netzkunst reconstructs, maintains, and preserves netart
+            and net culture. The association researches archiving and
+            contextualization of netart. Since 2019, we are based at the Haus
+            der Statisik, where we are conducting research on the history of the
+            building and its former functions.
+          </p>
+          <p>
+            Members of the association are:
+            <a
+              class="link"
+              target="_blank"
+              rel="noopener"
+              href="https://wwwwwwwwwwwwwwwwwwwwww.bitnik.org/"
+              >!Mediengruppe Bitnik</a
+            >, Tereza Havlíková,
+            <a
+              class="link"
+              target="_blank"
+              rel="noopener"
+              href="http://www.discombobulate.me/en/"
+              >Paloma Oliveira</a
+            >,
+            <a
+              href="https://super-filme.org/"
+              target="_blank"
+              rel="noopener"
+              class="link"
+              >Anneliese Ostertag</a
+            >,
+            <a
+              class="link"
+              target="_blank"
+              rel="noopener"
+              href="https://super-filme.org/"
+              >Tabea Rossol</a
+            >,
+            <a
+              class="link"
+              target="_blank"
+              rel="noopener"
+              href="http://www.curatingyoutube.net/"
+              >Sakrowski</a
+            >, and
+            <a
+              class="link"
+              target="_blank"
+              rel="noopener"
+              href="http://artwarez.org/"
+              >Cornelia Sollfrank</a
+            >.
+          </p>
         </template>
         <template v-else>
-          <p>Das Zentrum für Netzkunst rekonstruiert, erhält und bewahrt Netzkunst und Netzkultur. Der Verein erforscht Möglichkeiten der Archivierung und Kontextualisierung von Netzkunst. Seit September 2019 arbeiten wir als Pioniernutzer*innen am Haus der Statistik Berlin und forschen zu der Geschichte des Hauses.</p>
-          <p>Mitglieder des Vereins sind: <a class="link" target="_blank" rel="noopener" href="https://wwwwwwwwwwwwwwwwwwwwww.bitnik.org/">!Mediengruppe Bitnik</a>, Tereza Havlíková, <a class="link" target="_blank" rel="noopener" href="http://www.discombobulate.me/en/">Paloma Oliveira</a>, <a href="https://super-filme.org/" target="_blank" rel="noopener" class="link">Anneliese Ostertag</a>, <a class="link" target="_blank" rel="noopener" href="https://super-filme.org/">Tabea Rossol</a>, <a class="link" target="_blank" rel="noopener" href="http://www.curatingyoutube.net/">Sakrowski</a>, und <a class="link" target="_blank" rel="noopener" href="http://artwarez.org/">Cornelia Sollfrank</a>.</p>
+          <p>
+            Das Zentrum für Netzkunst rekonstruiert, erhält und bewahrt
+            Netzkunst und Netzkultur. Der Verein erforscht Möglichkeiten der
+            Archivierung und Kontextualisierung von Netzkunst. Seit September
+            2019 arbeiten wir als Pioniernutzer*innen am Haus der Statistik
+            Berlin und forschen zu der Geschichte des Hauses.
+          </p>
+          <p>
+            Mitglieder des Vereins sind:
+            <a
+              class="link"
+              target="_blank"
+              rel="noopener"
+              href="https://wwwwwwwwwwwwwwwwwwwwww.bitnik.org/"
+              >!Mediengruppe Bitnik</a
+            >, Tereza Havlíková,
+            <a
+              class="link"
+              target="_blank"
+              rel="noopener"
+              href="http://www.discombobulate.me/en/"
+              >Paloma Oliveira</a
+            >,
+            <a
+              href="https://super-filme.org/"
+              target="_blank"
+              rel="noopener"
+              class="link"
+              >Anneliese Ostertag</a
+            >,
+            <a
+              class="link"
+              target="_blank"
+              rel="noopener"
+              href="https://super-filme.org/"
+              >Tabea Rossol</a
+            >,
+            <a
+              class="link"
+              target="_blank"
+              rel="noopener"
+              href="http://www.curatingyoutube.net/"
+              >Sakrowski</a
+            >, und
+            <a
+              class="link"
+              target="_blank"
+              rel="noopener"
+              href="http://artwarez.org/"
+              >Cornelia Sollfrank</a
+            >.
+          </p>
         </template>
-        <img class="team__image" src="../assets/images/zfn.jpg" alt="Zentrum für Netzkunst Team" />
+        <img
+          class="team__image"
+          src="../assets/images/zfn.jpg"
+          alt="Zentrum für Netzkunst Team"
+        />
       </div>
       <div class="team__section-right">
         <a class="blob blob--green team__link" href="https://netzkunst.berlin/">
           netzkunst.berlin
         </a>
-        <a class="blob blob--green team__link" href="https://www.hausderstatistik.org">
+        <a
+          class="blob blob--green team__link"
+          href="https://www.hausderstatistik.org"
+        >
           hausderstatistik.org
         </a>
         <a class="blob blob--green team__link" href="https://www.panke.gallery">
@@ -39,87 +146,142 @@
       <div class="team__section-centre">
         <template v-if="english">
           <p>
-          <span>Concept and Curation</span><br>
-          <em>Tereza Havlíková, Paloma Oliveira, Anneliese Ostertag, Tabea Rossol, Sakrowski</em>
+            <span>Concept and Curation</span><br />
+            <em
+              >Tereza Havlíková, Paloma Oliveira, Anneliese Ostertag, Tabea
+              Rossol, Sakrowski</em
+            >
           </p>
           <p>
-          <span>Research, Graphic Concept, and Design</span><br>
-          <em><a class="link" target="_blank" rel="noopener" href="http://annaluiselorenz.com/">Anna-Luise Lorenz</a></em>
+            <span>Research, Graphic Concept, and Design</span><br />
+            <em
+              ><a
+                class="link"
+                target="_blank"
+                rel="noopener"
+                href="http://annaluiselorenz.com/"
+                >Anna-Luise Lorenz</a
+              ></em
+            >
           </p>
           <p>
-          <span>Programming</span><br>
-          <em><a class="link" target="_blank" rel="noopener" href="https://fiala.space">Jakub Fiala</a></em>
+            <span>Programming</span><br />
+            <em
+              ><a
+                class="link"
+                target="_blank"
+                rel="noopener"
+                href="https://fiala.space"
+                >Jakub Fiala</a
+              ></em
+            >
           </p>
           <p>
-          <span>Social Medial</span><br>
-          <em><a class="link" target="_blank" rel="noopener" href="https://intima.org">Igor Štromajer</a></em>
+            <span>Social Medial</span><br />
+            <em
+              ><a
+                class="link"
+                target="_blank"
+                rel="noopener"
+                href="https://intima.org"
+                >Igor Štromajer</a
+              ></em
+            >
           </p>
           <p>
-          <span>Editing and Translation</span><br>
-          <em>Colin Lang</em>
+            <span>Editing and Translation</span><br />
+            <em>Colin Lang</em>
+            <em>Paulina Thom</em>
           </p>
           <p>
-          <span>Symposium Assistance</span><br>
-          <em>Noemi Garay</em>
+            <span>Symposium Assistance</span><br />
+            <em>Noemi Garay</em>
           </p>
           <p>
-          <span>Exhibition Architecture</span><br>
-          <em>Omer Polak</em><br>
-          <em>Cäcilia Verweyen</em>
+            <span>Exhibition Architecture</span><br />
+            <em>Omer Polak</em><br />
+            <em>Cäcilia Verweyen</em>
           </p>
           <p>
-          <span>Sound Recording</span><br>
-          <em>Jürg Meister</em>
+            <span>Sound Recording</span><br />
+            <em>Jürg Meister</em>
           </p>
           <p>
-          <span>Documentation</span><br>
-          <em>Hannah Rumstedt</em><br>
-          <em>Alexia Manzano Schlamp</em>
+            <span>Documentation</span><br />
+            <em>Hannah Rumstedt</em><br />
+            <em>Alexia Manzano Schlamp</em>
           </p>
         </template>
         <template v-else>
           <p>
-          <span>Konzept und Kuration</span><br>
-          <em>Tereza Havlíková, Paloma Oliveira, Anneliese Ostertag, Tabea Rossol, Sakrowski</em>
+            <span>Konzept und Kuration</span><br />
+            <em
+              >Tereza Havlíková, Paloma Oliveira, Anneliese Ostertag, Tabea
+              Rossol, Sakrowski</em
+            >
           </p>
           <p>
-          <span>Recherche, Grafik Konzept und Design</span><br>
-          <em><a class="link" target="_blank" rel="noopener" href="http://annaluiselorenz.com/">Anna-Luise Lorenz</a></em>
+            <span>Recherche, Grafik Konzept und Design</span><br />
+            <em
+              ><a
+                class="link"
+                target="_blank"
+                rel="noopener"
+                href="http://annaluiselorenz.com/"
+                >Anna-Luise Lorenz</a
+              ></em
+            >
           </p>
           <p>
-          <span>Programmierung</span><br>
-          <em><a class="link" target="_blank" rel="noopener" href="https://fiala.space">Jakub Fiala</a></em>
+            <span>Programmierung</span><br />
+            <em
+              ><a
+                class="link"
+                target="_blank"
+                rel="noopener"
+                href="https://fiala.space"
+                >Jakub Fiala</a
+              ></em
+            >
           </p>
           <p>
-          <span>Soziale Medien</span><br>
-          <em><a class="link" target="_blank" rel="noopener" href="https://intima.org">Igor Štromajer</a></em>
+            <span>Soziale Medien</span><br />
+            <em
+              ><a
+                class="link"
+                target="_blank"
+                rel="noopener"
+                href="https://intima.org"
+                >Igor Štromajer</a
+              ></em
+            >
           </p>
           <p>
-          <span>Lektorat und Übersetzung</span><br>
-          <em>Colin Lang</em>
+            <span>Lektorat und Übersetzung</span><br />
+            <em>Colin Lang</em>
+            <em>Paulina Thom</em>
           </p>
           <p>
-          <span>Symposium Assistenz</span><br>
-          <em>Noemi Garay</em>
+            <span>Symposium Assistenz</span><br />
+            <em>Noemi Garay</em>
           </p>
           <p>
-          <span>Ausstellungsgestaltung</span><br>
-          <em>Omer Polak</em><br>
-          <em>Cäcilia Verweyen</em>
+            <span>Ausstellungsgestaltung</span><br />
+            <em>Omer Polak</em><br />
+            <em>Cäcilia Verweyen</em>
           </p>
           <p>
-          <span>Tonaufnahme</span><br>
-          <em>Jürg Meister</em>
+            <span>Tonaufnahme</span><br />
+            <em>Jürg Meister</em>
           </p>
           <p>
-          <span>Dokumentation</span><br>
-          <em>Hannah Rumstedt</em><br>
-          <em>Alexia Manzano Schlamp</em>
+            <span>Dokumentation</span><br />
+            <em>Hannah Rumstedt</em><br />
+            <em>Alexia Manzano Schlamp</em>
           </p>
         </template>
       </div>
-      <div class="team__section-right">
-      </div>
+      <div class="team__section-right"></div>
     </section>
     <SiteFooter />
   </main>
